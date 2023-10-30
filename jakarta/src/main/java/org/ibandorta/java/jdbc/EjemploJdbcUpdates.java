55330ab28280536b66309dffe452320103ffa661
ibandorta <ibandorta@gmail.com>
2023-10-30 17:54:43 +0000

    CRUD en BD

---
+++ jakarta/src/main/java/org/ibandorta/java/jdbc/EjemploJdbcUpdates.java
@@ -0,0 +1,48 @@
+package org.ibandorta.java.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
+
+import org.ibandorta.java.jdbc.models.Producto;
+import org.ibandorta.java.jdbc.repositorio.ProductoImpl;
+import org.ibandorta.java.jdbc.repositorio.Repositorio;
+import org.ibandorta.java.jdbc.util.ConexionBaseDatos;
+
+public class EjemploJdbcUpdates {
+
+	public static void main(String[] args) {
+
+
+
+		try (Connection	conn = ConexionBaseDatos.getInstance()) {
+
+
+		Repositorio<Producto>repositorio = new ProductoImpl();
+		System.out.println("=============listar================");
+		repositorio.listar().forEach(System.out::println);
+		
+		System.out.println("=============Obtener por id================");
+		System.out.println(repositorio.porId(2L));
+		
+		
+		System.out.println("=============Editar nuevo producto================");
+		Producto producto = new Producto();
+		producto.setId(3L);
+		producto.setNombre("teclado Razer");
+		producto.setPrecio(700);
+		repositorio.guardar(producto);		
+		System.out.println("Producto editado con éxito!!!!!!!");
+		repositorio.listar().forEach(System.out::println);
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
---
+++ jakarta/src/main/java/org/ibandorta/java/jdbc/util/ConexionBaseDatos.java
@@ -0,0 +1,22 @@
+package org.ibandorta.java.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionBaseDatos {
+	
+	private static String url = "jdbc:mysql://localhost:3306/java_curso";
+	private static String username = "root";
+	private static String password = "jidorta.1979";
+	private static Connection connection;
+	
+	public static Connection getInstance() throws SQLException {
+		if (connection == null) {
+			connection = DriverManager.getConnection(url, username, password);
+		}
+		return connection;
+	}
+
+
+}
