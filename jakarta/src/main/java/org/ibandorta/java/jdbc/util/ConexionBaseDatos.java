a3acd59ba60464847b056ebd6f3a76fadf44f43f
jidorta <126306110+jidorta@users.noreply.github.com>
2023-11-03 07:10:58 +0000

    Update ConexionBaseDatos.java

--- jakarta/src/main/java/org/ibandorta/java/jdbc/util/ConexionBaseDatos.java
+++ jakarta/src/main/java/org/ibandorta/java/jdbc/util/ConexionBaseDatos.java
@@ -8,7 +8,7 @@ public class ConexionBaseDatos {
 	
 	private static String url = "jdbc:mysql://localhost:3306/java_curso";
 	private static String username = "root";
-	private static String password = "jidorta.1979";
+	private static String password = "";
 	private static Connection connection;
 	
 	public static Connection getInstance() throws SQLException {
